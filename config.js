9c87e388d6d06c631412395af9a40a904ad20a28
Andrii Romanenko <roand@ukr.net>
2017-09-07 14:20:35 +0300

    changed MongoDB url from mlab.com

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     //путь к базе данных MongoDB
-    'mongoUrl' : 'mongodb://localhost:27017/todolists',
+    //'mongoUrl' : 'mongodb://localhost:27017/todolists',
+    'mongoUrl' : 'mongodb://roand@ukr.net:CompaqCQ58>@ds127894.mlab.com:27894/todos'
 }
