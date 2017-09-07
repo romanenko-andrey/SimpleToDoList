4ed971425ccd0cb0d1728d2837dc3fbf8509f141
Andrii Romanenko <roand@ukr.net>
2017-09-07 14:42:05 +0300

    changed userdb for MongoDB

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     //путь к базе данных MongoDB
     //'mongoUrl' : 'mongodb://localhost:27017/todolists',
-    'mongoUrl' : 'mongodb://roand@ukr.net:CompaqCQ58>@ds127894.mlab.com:27894/todos'
+    'mongoUrl' : 'mongodb://admin:123456@ds127894.mlab.com:27894/todos'
 }
